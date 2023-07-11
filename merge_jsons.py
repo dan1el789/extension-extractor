e8b5cf0754aa589d8c433924ec179e52851305ed
Daniel <48763469+dan1el789@users.noreply.github.com>
2023-07-11 19:26:34 +0200

    Bugfix for broken jsons (#2)
    * silence unzip
    
    * silence curl
    
    * improve logging

--- merge_jsons.py
+++ merge_jsons.py
@@ -5,9 +5,14 @@ data = []
 with open('manifests/extensions.csv', 'r') as extensions_csv:
     reader = csv.reader(extensions_csv)
     for row in reader:
-        with open('manifests/' + row[0] + '.json', 'r') as json_manifest:
-            manifest = json.load(json_manifest)
-            obj = {"id": row[0], "name": row[1], "manifest" : manifest}
+        print('manifests/' + row[0] + '.json')
+        try:
+            with open('manifests/' + row[0] + '.json', 'r') as json_manifest:
+                manifest = json.load(json_manifest)
+                obj = {"id": row[0], "name": row[1], "manifest" : manifest, "error" : "no"}
+                data.append(obj)
+        except:
+            obj = {"id": row[0], "name": row[1], "error" : "yes"}
             data.append(obj)
 
 out_file = open("output/manifests.json", "w")
