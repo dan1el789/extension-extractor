53eb1fdb0f2f68253a5fabecfc18e007f05a082b
Daniel <48763469+dan1el789@users.noreply.github.com>
2023-07-11 18:31:17 +0200

    Update merge_jsons.py

--- merge_jsons.py
+++ merge_jsons.py
@@ -2,15 +2,15 @@ import csv
 import json
 
 data = []
-with open('extensions.csv', 'r') as extensions_csv:
+with open('manifests/extensions.csv', 'r') as extensions_csv:
     reader = csv.reader(extensions_csv)
     for row in reader:
-        with open(row[0] + '/manifest.json', 'r') as json_manifest:
+        with open('manifests/' row[0] + '.json', 'r') as json_manifest:
             manifest = json.load(json_manifest)
             obj = {"id": row[0], "name": row[1], "manifest" : manifest}
             data.append(obj)
     print(data)
 
-out_file = open("manifests.json", "w")
+out_file = open("output/manifests.json", "w")
 json.dump(data, out_file)
 out_file.close()
