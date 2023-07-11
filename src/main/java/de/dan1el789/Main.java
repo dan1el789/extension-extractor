50bdcb4068a40af2f3722d554c3dbed7c1b8c2b3
Daniel <48763469+dan1el789@users.noreply.github.com>
2023-07-11 18:51:16 +0200

    Update Main.java

--- src/main/java/de/dan1el789/Main.java
+++ src/main/java/de/dan1el789/Main.java
@@ -2,8 +2,7 @@ package de.dan1el789;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Downloader d = new Downloader(2,"https://addons.mozilla.org/de/firefox/search/?sort=users&type=extension&page=");
+        Downloader d = new Downloader(40,"https://addons.mozilla.org/de/firefox/search/?sort=users&type=extension&page=");
         d.downloadExtensions();
         d.saveToFile("extensions.csv");
     }
