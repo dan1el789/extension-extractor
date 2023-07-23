30ce818cfe14ca7d3a13df580c69584326ab86c3
dan1el789 <dan1el789@users.noreply.github.com>
2023-07-23 11:29:42 +0000

    deploy: 0b311fe4e7d71da0bd263a0424e78a3cd491b922

---
+++ dateUtil.js
@@ -0,0 +1,28 @@
+function parseDate(dateString){
+    dateString = dateString.split('(')[1].replaceAll(')','')
+    let day = dateString.split(" ")[0].replace(".", "");
+    let month = ["Jan.", "Feb.", "März", "Apr.", "Mai", 
+        "Juni", "Juli", "Aug.", "Sep.", "Okt.", "Nov.", "Dez."]
+        .lastIndexOf(dateString.split(" ")[1]) + 1;
+    let year = dateString.split(" ")[2];
+
+    return new Date(Date.parse(year + "-" + month + "-" + day));
+}
+
+function createDateList(){
+    let dates = [];
+    dates.push()
+    manifests
+        .filter(manifest => manifest.lastUpdate)
+        .forEach(manifest => dates
+            .push(parseDate(manifest.lastUpdate))
+        )
+    dates.sort((a,b) => a.valueOf() < b.valueOf())
+    return dates;
+}
+
+Date.prototype.addDays = function(days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
